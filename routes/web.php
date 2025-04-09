86acd20910e00533e347a9885bed7f9844f36629
RafaDevCS <csitajuba1@gmail.com>
2025-04-09 15:21:35 -0300

    Removi pasta minio

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,3 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
-
-    return ['token' => $token->plainTextToken];
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
